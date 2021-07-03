d0d7dc22ce7ba1e5cef9281451b1377ba06a349b
Fábio Lameira <fabiolameira97@gmail.com>
2021-07-03 10:44:50 +0100

    Adding timer read values and calculate acceleration every 10 ms.

--- eFall/eFall/eFall.ino
+++ eFall/eFall/eFall.ino
@@ -1,47 +1,73 @@
 #include "MIDDLE.h"
+#include "NRF52_MBED_TimerInterrupt.h"
+#include "NRF52_MBED_ISR_Timer.h"
 
+// Timer Configs
+#define TIMER_INTERVAL_MS 10
+NRF52_MBED_Timer ITimer(NRF_TIMER_3);
+NRF52_MBED_ISR_Timer ISR_Timer;
+volatile char flagTimer = 0;
+
+// MIDDLE class instance
 MIDDLE middle = MIDDLE();
 
 void setup() {
-	// SERIAL Comm Init
+	// SERIAL Initialization
 	Serial.begin(9600);
 	delay(100);
 	
-	Serial.println("Estou vivo.");
-	
-	// IMU Init
+	// IMU Initialization
 	Serial.println("IMU initializing...");
 	int state = middle.begin();
 	if (state)
 		Serial.println("IMU initialized");
 	else
 		Serial.println("IMU Error");
+		
+	// TIMER Initialization
+	if (ITimer.attachInterruptInterval(1000, timerHandler)) {
+		Serial.print(F("Starting ITimer OK, millis() = ")); Serial.println(millis());
+	} else
+		Serial.println(F("Can't set ITimer. Select another freq. or timer"));
+	
+	ISR_Timer.setInterval(TIMER_INTERVAL_MS,  changeFlagTimer);
 }
 
 void loop() {
 	
-	middle.readValues();
-	middle.feedFifoX(middle.getX());
-	middle.feedFifoY(middle.getY());
-	middle.feedFifoZ(middle.getZ());
-	middle.feedFifoA();
-	middle.feedFifoM();
+	if (flagTimer == 1) {	
+		middle.readValues();
+		middle.feedFifoX(middle.getX());
+		middle.feedFifoY(middle.getY());
+		middle.feedFifoZ(middle.getZ());
+		middle.feedFifoA();
+		middle.feedFifoM();
+		flagTimer = 0;
+	}
 	
 	if (Serial.available() > 0) {
 		switch (Serial.read()) {
-		case '1':
-			Serial.println("Carregaste no 1");
-			middle.printFifos();
-			break;
-		case '2':
-			Serial.println("Carregaste no 2");
-			middle.printFifoM();
-			break;
-		case '3':
-			Serial.println("Carregaste no 3");
-			break;
-		default:
-			break;
+			case '1':
+				Serial.println("Carregaste no 1");
+				middle.printFifos();
+				break;
+			case '2':
+				Serial.println("Carregaste no 2");
+				middle.printFifoM();
+				break;
+			case '3':
+				Serial.println("Carregaste no 3");
+				break;
+			default:
+				break;
 		}
 	}
+}
+
+void timerHandler() {
+	ISR_Timer.run();
+}
+
+void changeFlagTimer() {
+	flagTimer = 1;
 }
